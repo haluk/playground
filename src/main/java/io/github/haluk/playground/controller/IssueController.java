fb430db03e5bbfc186fd071bff464a865a01058d
haluk <hdogan@vivaldi.net>
2020-02-11 15:29:41 -0600

    update issue controller and template

--- src/main/java/io/github/haluk/playground/controller/IssueController.java
+++ src/main/java/io/github/haluk/playground/controller/IssueController.java
@@ -1,28 +1,29 @@
 package io.github.haluk.playground.controller;
 
+import io.github.haluk.playground.entity.IssueReport;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class IssueController {
 
     @GetMapping("/issuereport")
-    @ResponseBody
-    public String getReport() {
+    public String getReport(Model model) {
+        model.addAttribute("issuereport", new IssueReport());
         return "issues/issuereport_form";
     }
 
     @PostMapping("/issuereport")
-    @ResponseBody
-    public String submitReport() {
+    public String submitReport(IssueReport issueReport, Model model) {
+        model.addAttribute("issuereport", new IssueReport());
+        model.addAttribute("submitted", true);
         return "issues/issuereport_form";
     }
 
     @GetMapping("/issues")
-    @ResponseBody
-    public String getIssues() {
+    public String getIssues(Model model) {
         return "issues/issuereport_list";
     }
 }
